d9751dbc10071eba0c7ed8f634b114747e10e83e
JamesCaud <jcaudill914@gmail.com>
2020-10-10 22:05:08 -0400

    test commit and adding comment stubs

---
+++ trading.py
@@ -0,0 +1,30 @@
+"""
+Trading in Python
+
+Author: JamesCaud
+Start Date: 10/10/20
+Last Commit: 10/10/20
+
+"""
+
+
+
+# Imports
+
+# Globals
+
+# Classes
+
+# Functions
+
+def main():
+    """
+    main function to begin execution of trading algorithms
+
+    """
+    return
+
+
+if "__name__" == "__main__":
+    main()
+    return
